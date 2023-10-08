61a7b8be722ca3a701143e91f519d1f52dfd2a95
Ershad98 <rashoodah.1090908@gmail.com>
2023-10-08 12:10:11 +0300

    first commit

---
+++ firstlearning/Controllers/Hellowordcontroller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using firstlearning.Models;
+namespace firstlearning.Controllers
+{
+    public class Hellowordcontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel() { Name = "sif",Age =12};
+            return View(doggo);
+        }
+        public IActionResult test ()
+        {
+            return View();
+        }
+    }
+}
