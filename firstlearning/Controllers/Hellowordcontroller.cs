1e1ce34142fc87cd76801c49fc85f3e70e48392d
Ershad98 <96366812+Ershad98@users.noreply.github.com>
2023-10-09 11:57:30 +0300

    saving value from form

--- firstlearning/Controllers/Hellowordcontroller.cs
+++ firstlearning/Controllers/Hellowordcontroller.cs
@@ -4,14 +4,21 @@ namespace firstlearning.Controllers
 {
     public class Hellowordcontroller : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         {
-            DogViewModel doggo = new DogViewModel() { Name = "sif",Age =12};
-            return View(doggo);
+           // DogViewModel doggo = new DogViewModel() { Name = "sif",Age =   2};
+            return View(dogs);
         }
-        public IActionResult test ()
+        public IActionResult create ()
         {
-            return View();
+            var dogvm = new DogViewModel();
+            return View(dogvm);
+        }
+        public IActionResult createDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
